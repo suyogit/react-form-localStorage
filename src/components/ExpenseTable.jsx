846277fea977ca9a23cdba29b4e89a232bd4697b
Suyog Acharya <asuyog042@gmail.com>
2024-10-17 19:14:58 +0545

    sorting implemented

--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import { useFilter } from "./hooks/useFilter";
 import ContextMenu from "./ContextMenu";
-const ExpenseTable = ({ expenses ,setexpense, setTdata, setediting}) => {
-  const [rowid, setrowid] = useState('')
-  const [menuposition, setmenuposition] = useState({})
+const ExpenseTable = ({ expenses, setexpense, setTdata, setediting }) => {
+  const [rowid, setrowid] = useState("");
+  const [menuposition, setmenuposition] = useState({});
   const [filteredData, setQuery] = useFilter(expenses, (data) => data.category); //now we can filter on the basis of field that we desire
   function handleChange(e) {
     setQuery(e.target.value.toLowerCase());
@@ -14,8 +14,21 @@ const ExpenseTable = ({ expenses ,setexpense, setTdata, setediting}) => {
   );
   return (
     <>
-      <ContextMenu  menuposition={menuposition} setmenuposition={setmenuposition} setexpense={setexpense} expenses={expenses} rowid={rowid} setTdata={setTdata} setediting={setediting}/>
-      <table className="expense-table" onClick={()=>setmenuposition({})}>
+      <ContextMenu
+        menuposition={menuposition}
+        setmenuposition={setmenuposition}
+        setexpense={setexpense}
+        expenses={expenses}
+        rowid={rowid}
+        setTdata={setTdata}
+        setediting={setediting}
+      />
+      <table
+        className="expense-table"
+        onClick={() => {
+          if (menuposition.left) setmenuposition({});
+        }}
+      >
         <thead>
           <tr>
             <th>Title</th>
@@ -37,6 +50,11 @@ const ExpenseTable = ({ expenses ,setexpense, setTdata, setediting}) => {
                   width="10"
                   viewBox="0 0 384 512"
                   className="arrow up-arrow"
+                  onClick={() => {
+                    setexpense((prev) => [
+                      ...prev.sort((a, b) => a.price - b.price),
+                    ]);
+                  }}
                 >
                   <title>Ascending</title>
                   <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" />
@@ -46,6 +64,11 @@ const ExpenseTable = ({ expenses ,setexpense, setTdata, setediting}) => {
                   width="10"
                   viewBox="0 0 384 512"
                   className="arrow down-arrow"
+                  onClick={() => {
+                    setexpense((prev) => [
+                      ...prev.sort((a, b) => b.price - a.price),
+                    ]);
+                  }}
                 >
                   <title>Descending</title>
                   <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
@@ -60,8 +83,8 @@ const ExpenseTable = ({ expenses ,setexpense, setTdata, setediting}) => {
               key={id}
               onContextMenu={(e) => {
                 e.preventDefault();
-                setmenuposition({left:e.clientX+4, top:e.clientY+1})
-                setrowid(id)
+                setmenuposition({ left: e.clientX + 4, top: e.clientY + 1 });
+                setrowid(id);
               }}
             >
               <td>{title}</td>
