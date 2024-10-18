8e2c18525cab37fd00ecd760c2101bac2908747f
Suyog Acharya <asuyog042@gmail.com>
2024-10-18 13:36:42 +0545

    additional info added

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # React + Vite
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+#Features
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+1. Local Storage CRUD Operations: Key feature allowing editing and deleting data stored in local storage using React.
+2. Form Validation: Ensures accurate and secure data entry in the form.
+3. Custom Hooks: Efficient filtering and local storage management through reusable React hooks.
+4. Context Menu Integration: Provides right-click options for user interactions.
+5. Custom Input and Select Components: Tailored input fields and dropdowns for enhanced user experience.
+6. Sorting Functionality: Enables organized data display for better usability.
--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,51 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
 // import ContextMenu from './components/ContextMenu'
-import data from './components/data'
-import ExpenseForm from './components/ExpenseForm'
-import ExpenseTable from './components/ExpenseTable'
-import { useLocalStorage } from './components/hooks/useLocalStorage'
-
+import data from "./components/data";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseTable from "./components/ExpenseTable";
+import { useLocalStorage } from "./components/hooks/useLocalStorage";
 
 function App() {
   // const [expense, setexpense] = useState(data)
-  const [editing, setediting]=useLocalStorage('editing','')
-  const [tdata, setTdata] = useLocalStorage('tdata',{
+  const [editing, setediting] = useLocalStorage("editing", "");
+  const [tdata, setTdata] = useLocalStorage("tdata", {
     title: "",
     category: "",
     price: "",
     // email: "",
   });
 
-  const [expense, setexpense]=useLocalStorage('expense', data)
+  const [expense, setexpense] = useLocalStorage("expense", data);
   // console.log(localData)
-
 
   return (
     <>
       <main>
-      <h1>Track Your Expense</h1>
-    
-      <div className="expense-tracker">
-        <ExpenseForm setexpense={setexpense} tdata={tdata} setTdata={setTdata}  editing={editing} setediting={setediting}/>
-        <ExpenseTable expenses={expense} setexpense={setexpense} setTdata={setTdata} setediting={setediting}/>
-      </div>
-    </main>
-     
+        <h1 className="title">Track Your Expense</h1>
+
+        <div className="expense-tracker">
+          <ExpenseForm
+            setexpense={setexpense}
+            tdata={tdata}
+            setTdata={setTdata}
+            editing={editing}
+            setediting={setediting}
+          />
+          <ExpenseTable
+            expenses={expense}
+            setexpense={setexpense}
+            setTdata={setTdata}
+            setediting={setediting}
+          />
+        </div>
+      </main>
+      <h5
+        style={{ textAlign: "center", margin: 0, padding: 0, fontWeight: 100 }}
+      >
+        <i>(Right click to edit or delete row)</i>
+      </h5>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
